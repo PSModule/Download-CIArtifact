80ad1a630130589af8dc37b94c2f2bcea1e13d1d
Marius Storhaug <marstor@hotmail.com>
2024-08-10 18:51:10 +0200

    initial code

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -4,4 +4,13 @@ param(
     [string] $Subject = $env:GITHUB_ACTION_INPUT_subject
 )
 
-Write-Host "Hello, $subject!"
+Write-Output "::group::Get PR"
+$PR = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$env:GITHUB_REPOSITORY/commits/$env:GITHUB_SHA/pulls" | ConvertFrom-Json
+$PR  | ConvertTo-Json -Depth 100
+
+Write-Output "::group::Get WorkflowRun"
+$WorkflowRuns = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$env:GITHUB_REPOSITORY/actions/workflows/CI.yml/runs?head_sha=$($PR.head.sha)" | ConvertFrom-Json | Select-Object -ExpandProperty workflow_runs
+$WorkflowRuns | ConvertTo-Json -Depth 100
+Write-Output "::endgroup::"
+
+"run-id=$($WorkflowRuns.id)" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
