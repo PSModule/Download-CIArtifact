b7a7db3a12f2b73aca9c5484781e1888be053cd4
Marius Storhaug <marstor@hotmail.com>
2024-08-10 19:20:31 +0200

    Fix

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,16 +1,36 @@
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName = 'WorkflowID')]
 param(
+    # The ID of the workflow to get the run for.
     [Parameter()]
-    [string] $Subject = $env:GITHUB_ACTION_INPUT_subject
+    [string] $WorkflowID = $env:GITHUB_ACTION_INPUT_WorkflowID,
+
+    # The ID of the workflow run to verify.
+    [Parameter()]
+    [string] $WorkflowRunID = $env:GITHUB_ACTION_INPUT_WorkflowRunID
 )
 
-Write-Output "::group::Get PR"
-$PR = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$env:GITHUB_REPOSITORY/commits/$env:GITHUB_SHA/pulls" | ConvertFrom-Json
-$PR  | ConvertTo-Json -Depth 100
+if ($WorkflowRunID) {
+    Write-Output '::group::Verify Workflow Run'
+    gh api -H 'Accept: application/vnd.github+json' -H 'X-GitHub-Api-Version: 2022-11-28' "/repos/$env:GITHUB_REPOSITORY/actions/runs/$WorkflowRunID"
+    "RunID=$WorkflowRunID" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+    Write-Output '::endgroup::'
+    return
+} else {
+    Write-Output '::group::Get PR'
+    $PR = gh api -H 'Accept: application/vnd.github+json' -H 'X-GitHub-Api-Version: 2022-11-28' `
+        "/repos/$env:GITHUB_REPOSITORY/commits/$env:GITHUB_SHA/pulls" | ConvertFrom-Json
+    $PR | ConvertTo-Json -Depth 100
+    Write-Output '::endgroup::'
 
-Write-Output "::group::Get WorkflowRun"
-$WorkflowRuns = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$env:GITHUB_REPOSITORY/actions/workflows/CI.yml/runs?head_sha=$($PR.head.sha)" | ConvertFrom-Json | Select-Object -ExpandProperty workflow_runs
-$WorkflowRuns | ConvertTo-Json -Depth 100
-Write-Output "::endgroup::"
+    Write-Output '::group::Get WorkflowRun'
+    $WorkflowRuns = gh api -H 'Accept: application/vnd.github+json' -H 'X-GitHub-Api-Version: 2022-11-28' `
+        "/repos/$env:GITHUB_REPOSITORY/actions/workflows/$WorkflowID/runs?head_sha=$($PR.head.sha)" |
+        ConvertFrom-Json | Select-Object -ExpandProperty workflow_runs
+    $WorkflowRuns | ConvertTo-Json -Depth 100
+    $WorkflowRunID = $WorkflowRuns.id
+    Write-Output '::endgroup::'
+}
 
-"run-id=$($WorkflowRuns.id)" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+Write-Output "Workflow Run ID: [$WorkflowRunID]"
+
+"RunID=$WorkflowRunID" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
